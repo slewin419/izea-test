9e52e8a228b4a6e8023b723c83b0079e6d1d03d0
Shaun Lewin <info@shaunlewin.com>
2019-05-06 20:53:25 -0400

    refresh post model when query params change

--- app/routes/posts.js
+++ app/routes/posts.js
@@ -3,12 +3,20 @@ import {inject as service } from '@ember/service';
 
 export default Route.extend({
     posts: Ember.inject.service(),
+    queryParams: {
+        page: {
+            refreshModel: true
+        },
+        size: {
+            refreshModel: true
+        }
+    },
     model(params) {
         //debugger;
         return this.get('posts').request('/posts', {
             data: {
                 _start: params.size * (--params.page),
-                _limit: params.size * (++params.page)
+                _limit: params.size
             }
         });
     }
